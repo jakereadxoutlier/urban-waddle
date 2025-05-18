861bf39e09a18b03452064a4168816968f4fc2dc
jakereadxoutlier <jake@outlierestates.com>
2025-05-18 07:30:09 -0700

    Add Expo entrypoint

---
+++ App.js
@@ -0,0 +1,6 @@
+// App.js
+import { registerRootComponent } from 'expo';
+import App from './index';         // or './src/App' if your main component lives there
+
+// This runs your Expo app:
+registerRootComponent(App);
